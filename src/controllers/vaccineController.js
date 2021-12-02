6f8d5705ac86844b2a37ced20bfcb2885f3f074b
FlaviaDosReis <reis.dos.flavia@gmail.com>
2021-12-02 00:46:57 -0300

    Vaccine Control - Final Version

--- src/controllers/vaccineController.js
+++ src/controllers/vaccineController.js
@@ -11,6 +11,74 @@ const createVaccine = async (req, res) => {
     }
 }
 
+const getAllVaccine = async (req, res) => {
+    const vaccinated = req.query.vaccinated
+    try {
+        const where = vaccinated ? { where: { vaccinated } } : {}
+
+        const vaccine = await Vaccine.findAll(where)
+        if (vaccine && vaccine.length > 0) {
+            res.status(200).send(vaccine)
+        } else {
+            res.status(204).send()
+        }
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
+}
+
+const getByIdVaccine = async (req, res) => {
+    const vaccineId = req.params.id
+    try {
+        const vaccine = await Vaccine.findOne({
+            where: {id:vaccineId }
+        });
+        if (vaccine){
+            res.status(200).send(vaccine)
+        } else {
+            res.status(404).send({message: `A vacina referente ao id: ${vaccineId} não foi encontrada.`})
+        }
+    } catch (error) {
+        res.status(500).send({message: error.message })
+
+    }
+}
+
+const updateVaccine = async (req, res) => {
+    const vaccineId = req.params.id
+    const  vaccinated  = req.body.vaccinated
+    try {
+        const rowsUpdate = await Vaccine.update ({ vaccinated }, {
+            where: {id:vaccineId}
+        });
+        if (rowsUpdate && rowsUpdate > 0){
+            res.status(200).send({message: `A vacina com o id: id: ${vaccineId} teve a situação de aplicada alterada.`})
+        } else {
+            res.status(404).send({message: `Não encontramos a vacina com id: ${vaccineId} para atualizá-la.`})
+        }
+    } catch (error) {
+        res.status(500).send({message: error.message })
+    }
+}
+
+const deleteVaccine = async (req, res) => {
+    const vaccineId = req.params.id
+    try{
+        const rowsDelete = await Vaccine.destroy ({where: {id: vaccineId } });
+        if (rowsDelete) {
+            res.status(200).send({message: `${rowsDelete} vacina(s) deletada(s).`})
+        } else {
+            res.status(404).send({message: `Não foi possível deletar a vacina com id: ${vaccineId}.`})
+        }
+    } catch (error) {
+        res.status(500).send({message: error.message })
+    }
+}
+
 module.exports = {
     createVaccine,
+    getAllVaccine,
+    getByIdVaccine,
+    updateVaccine,
+    deleteVaccine
 }
--- src/routes/vaccines.js
+++ src/routes/vaccines.js
@@ -3,5 +3,9 @@ const router = express.Router()
 const controller = require("../controllers/vaccineController")
 
 router.post("/", controller.createVaccine)
+router.get("/", controller.getAllVaccine)
+router.get("/:id", controller.getByIdVaccine)
+router.patch("/:id/vaccinated", controller.updateVaccine)
+router.delete("/:id", controller.deleteVaccine)
 
 module.exports = router
